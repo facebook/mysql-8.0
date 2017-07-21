facebook/mysql-5.6

const fix for the rest of the files under storage/rocksdb after rebase
Summary:
This is the last pull request for the const fix of all the files under storage/rocksdb.

The changes will improve the robustness of the code and make it easier for refactoring in the future.
Closes https://github.com/facebook/mysql-5.6/pull/480

Differential Revision: D4349370

Pulled By: sidigit1205

fbshipit-source-id: bbe7963

storage/rocksdb/rdb_cf_manager.h
@@ -55,7 +55,8 @@ class Rdb_cf_manager
 
   static
   void get_per_index_cf_name(const std::string& db_table_name,
-                             const char *index_name, std::string *res);
+                             const char* const index_name,
+                             std::string* const res);
 
   Rdb_cf_options* m_cf_options= nullptr;
 
@@ -64,14 +65,14 @@ public:
   Rdb_cf_manager& operator=(const Rdb_cf_manager&) = delete;
   Rdb_cf_manager() = default;
 
-  static bool is_cf_name_reverse(const char *name);
+  static bool is_cf_name_reverse(const char* const name);
 
   /*
     This is called right after the DB::Open() call. The parameters describe column
     families that are present in the database. The first CF is the default CF.
   */
   void init(Rdb_cf_options* cf_options,
-            std::vector<rocksdb::ColumnFamilyHandle*> *handles);
+            std::vector<rocksdb::ColumnFamilyHandle*>* const handles);
   void cleanup();
 
   /*
@@ -80,17 +81,18 @@ public:
     - cf_name=_auto_ means use 'dbname.tablename.indexname'
   */
   rocksdb::ColumnFamilyHandle* get_or_create_cf(
-      rocksdb::DB *rdb, const char *cf_name, const std::string& db_table_name,
-      const char *index_name, bool *is_automatic);
+      rocksdb::DB* const rdb, const char *cf_name,
+      const std::string& db_table_name, const char* const index_name,
+      bool* const is_automatic);
 
   /* Used by table open */
   rocksdb::ColumnFamilyHandle* get_cf(const char *cf_name,
                                       const std::string& db_table_name,
-                                      const char *index_name,
-                                      bool *is_automatic) const;
+                                      const char* const index_name,
+                                      bool* const is_automatic) const;
 
   /* Look up cf by id; used by datadic */
-  rocksdb::ColumnFamilyHandle* get_cf(const uint32_t id) const;
+  rocksdb::ColumnFamilyHandle* get_cf(const uint32_t &id) const;
 
   /* Used to iterate over column families for show status */
   std::vector<std::string> get_cf_names(void) const;
@@ -102,7 +104,7 @@ public:
 
   void get_cf_options(
     const std::string &cf_name,
-    rocksdb::ColumnFamilyOptions *opts) __attribute__((__nonnull__)) {
+    rocksdb::ColumnFamilyOptions* const opts) __attribute__((__nonnull__)) {
       m_cf_options->get_cf_options(cf_name, opts);
   }
 };

storage/rocksdb/rdb_cf_options.h
@@ -45,13 +45,13 @@ class Rdb_cf_options
   Rdb_cf_options& operator=(const Rdb_cf_options&) = delete;
   Rdb_cf_options() = default;
 
-  void get(const std::string &cf_name, rocksdb::ColumnFamilyOptions *opts);
+  void get(const std::string &cf_name, rocksdb::ColumnFamilyOptions* const opts);
 
   bool init(
     const rocksdb::BlockBasedTableOptions& table_options,
     std::shared_ptr<rocksdb::TablePropertiesCollectorFactory> prop_coll_factory,
-    const char * default_cf_options,
-    const char * override_cf_options);
+    const char* const default_cf_options,
+    const char* const override_cf_options);
 
   const rocksdb::ColumnFamilyOptions& get_defaults() const {
     return m_default_cf_opts;
@@ -62,20 +62,21 @@ class Rdb_cf_options
 
   void get_cf_options(
     const std::string &cf_name,
-    rocksdb::ColumnFamilyOptions *opts) __attribute__((__nonnull__));
+    rocksdb::ColumnFamilyOptions* const opts) __attribute__((__nonnull__));
 
  private:
   bool set_default(const std::string &default_config);
   bool set_override(const std::string &overide_config);
 
   /* Helper string manipulation functions */
-  static void skip_spaces(const std::string& input, size_t* pos);
-  static bool find_column_family(const std::string& input, size_t* pos,
-                                 std::string* key);
-  static bool find_options(const std::string& input, size_t* pos,
-                           std::string* options);
-  static bool find_cf_options_pair(const std::string& input, size_t* pos,
-                                   std::string* cf, std::string* opt_str);
+  static void skip_spaces(const std::string& input, size_t* const pos);
+  static bool find_column_family(const std::string& input, size_t* const pos,
+                                 std::string* const key);
+  static bool find_options(const std::string& input, size_t* const pos,
+                           std::string* const options);
+  static bool find_cf_options_pair(const std::string& input, size_t* const pos,
+                                   std::string* const cf,
+                                   std::string* const opt_str);
 
  private:
   static Rdb_pk_comparator s_pk_comparator;

facebook/mysql-8.0

storage/rocksdb/rdb_cf_manager.h
@@ -55,7 +55,8 @@ class Rdb_cf_manager
 
   static
   void get_per_index_cf_name(const std::string& db_table_name,
-                             const char *index_name, std::string *res);
+                             const char* const index_name,
+                             std::string* const res);
 
   Rdb_cf_options* m_cf_options= nullptr;
 
@@ -64,14 +65,14 @@ public:
   Rdb_cf_manager& operator=(const Rdb_cf_manager&) = delete;
   Rdb_cf_manager() = default;
 
-  static bool is_cf_name_reverse(const char *name);
+  static bool is_cf_name_reverse(const char* const name);
 
   /*
     This is called right after the DB::Open() call. The parameters describe column
     families that are present in the database. The first CF is the default CF.
   */
   void init(Rdb_cf_options* cf_options,
-            std::vector<rocksdb::ColumnFamilyHandle*> *handles);
+            std::vector<rocksdb::ColumnFamilyHandle*>* const handles);
   void cleanup();
 
   /*
@@ -80,17 +81,18 @@ public:
     - cf_name=_auto_ means use 'dbname.tablename.indexname'
   */
   rocksdb::ColumnFamilyHandle* get_or_create_cf(
-      rocksdb::DB *rdb, const char *cf_name, const std::string& db_table_name,
-      const char *index_name, bool *is_automatic);
+      rocksdb::DB* const rdb, const char *cf_name,
+      const std::string& db_table_name, const char* const index_name,
+      bool* const is_automatic);
 
   /* Used by table open */
   rocksdb::ColumnFamilyHandle* get_cf(const char *cf_name,
                                       const std::string& db_table_name,
-                                      const char *index_name,
-                                      bool *is_automatic) const;
+                                      const char* const index_name,
+                                      bool* const is_automatic) const;
 
   /* Look up cf by id; used by datadic */
-  rocksdb::ColumnFamilyHandle* get_cf(const uint32_t id) const;
+  rocksdb::ColumnFamilyHandle* get_cf(const uint32_t &id) const;
 
   /* Used to iterate over column families for show status */
   std::vector<std::string> get_cf_names(void) const;
@@ -102,7 +104,7 @@ public:
 
   void get_cf_options(
     const std::string &cf_name,
-    rocksdb::ColumnFamilyOptions *opts) MY_ATTRIBUTE((__nonnull__)) {
+    rocksdb::ColumnFamilyOptions* const opts) MY_ATTRIBUTE((__nonnull__)) {
       m_cf_options->get_cf_options(cf_name, opts);
   }
 };

storage/rocksdb/rdb_cf_options.h
@@ -45,13 +45,13 @@ class Rdb_cf_options
   Rdb_cf_options& operator=(const Rdb_cf_options&) = delete;
   Rdb_cf_options() = default;
 
-  void get(const std::string &cf_name, rocksdb::ColumnFamilyOptions *opts);
+  void get(const std::string &cf_name, rocksdb::ColumnFamilyOptions* const opts);
 
   bool init(
     const rocksdb::BlockBasedTableOptions& table_options,
     std::shared_ptr<rocksdb::TablePropertiesCollectorFactory> prop_coll_factory,
-    const char * default_cf_options,
-    const char * override_cf_options);
+    const char* const default_cf_options,
+    const char* const override_cf_options);
 
   const rocksdb::ColumnFamilyOptions& get_defaults() const {
     return m_default_cf_opts;
@@ -62,20 +62,21 @@ class Rdb_cf_options
 
   void get_cf_options(
     const std::string &cf_name,
-    rocksdb::ColumnFamilyOptions *opts) MY_ATTRIBUTE((__nonnull__));
+    rocksdb::ColumnFamilyOptions* const opts) MY_ATTRIBUTE((__nonnull__));
 
  private:
   bool set_default(const std::string &default_config);
   bool set_override(const std::string &overide_config);
 
   /* Helper string manipulation functions */
-  static void skip_spaces(const std::string& input, size_t* pos);
-  static bool find_column_family(const std::string& input, size_t* pos,
-                                 std::string* key);
-  static bool find_options(const std::string& input, size_t* pos,
-                           std::string* options);
-  static bool find_cf_options_pair(const std::string& input, size_t* pos,
-                                   std::string* cf, std::string* opt_str);
+  static void skip_spaces(const std::string& input, size_t* const pos);
+  static bool find_column_family(const std::string& input, size_t* const pos,
+                                 std::string* const key);
+  static bool find_options(const std::string& input, size_t* const pos,
+                           std::string* const options);
+  static bool find_cf_options_pair(const std::string& input, size_t* const pos,
+                                   std::string* const cf,
+                                   std::string* const opt_str);
 
  private:
   static Rdb_pk_comparator s_pk_comparator;