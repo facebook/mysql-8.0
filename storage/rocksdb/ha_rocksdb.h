percona/percona-server

String handling changes
Summary: Changed string handling to facilitate https://reviews.facebook.net/D59643

Test Plan: MTR

Reviewers: hermanlee4

Reviewed By: hermanlee4

Subscribers: webscalesql-eng

Differential Revision: https://reviews.facebook.net/D60687

storage/rocksdb/ha_rocksdb.h
@@ -377,10 +377,10 @@ class ha_rocksdb: public my_core::handler
   */
   int m_dupp_errkey;
 
-  int create_key_defs(TABLE *table_arg, const char *db_table, const uint len,
+  int create_key_defs(const TABLE *table_arg, const std::string& db_table,
                       HA_CREATE_INFO *create_info, Rdb_tbl_def *tbl_def_arg,
-                      Rdb_tbl_def *old_tbl_def_arg= nullptr)
-      __attribute__((__nonnull__(2, 3, 5, 6), __warn_unused_result__));
+                      const Rdb_tbl_def *old_tbl_def_arg= nullptr)
+      __attribute__((__nonnull__(2, 4, 5), __warn_unused_result__));
   int secondary_index_read(const int keyno, uchar *buf)
       __attribute__((__nonnull__, __warn_unused_result__));
   void setup_iterator_for_rnd_scan();
@@ -701,18 +701,19 @@ private:
     bool             skip_unique_check;
   };
 
-  int create_cfs(TABLE *table_arg, const char *db_table,
+  int create_cfs(const TABLE *table_arg, const std::string& db_table,
                  Rdb_tbl_def *tbl_def_arg,
                  std::array<struct key_def_cf_info, MAX_INDEXES + 1>* cfs);
 
 
-  int create_key_def(TABLE *table_arg, uint i, const Rdb_tbl_def* tbl_def_arg,
+  int create_key_def(const TABLE *table_arg, uint i,
+                     const Rdb_tbl_def* tbl_def_arg,
                      std::shared_ptr<Rdb_key_def>* new_key_def,
                      const struct key_def_cf_info& cf_info);
 
-  int create_inplace_key_defs(TABLE *table_arg,
+  int create_inplace_key_defs(const TABLE *table_arg,
                        Rdb_tbl_def *tbl_def_arg,
-                       Rdb_tbl_def *old_tbl_def_arg,
+                       const Rdb_tbl_def *old_tbl_def_arg,
                        const std::array<key_def_cf_info, MAX_INDEXES + 1>& cfs);
 
   int index_first_intern(uchar *buf)

storage/rocksdb/rdb_datadic.h
@@ -344,7 +344,7 @@ public:
     SECONDARY_FORMAT_VERSION_LATEST= SECONDARY_FORMAT_VERSION_INITIAL,
   };
 
-  void setup(TABLE *table, Rdb_tbl_def *tbl_def);
+  void setup(const TABLE *table, const Rdb_tbl_def *tbl_def);
 
   rocksdb::ColumnFamilyHandle *get_cf() const { return m_cf_handle; }
 
@@ -620,7 +620,7 @@ public:
   ~Rdb_tbl_def();
 
   /* Stores 'dbname.tablename' */
-  StringBuffer<64> m_dbname_tablename;
+  std::string m_dbname_tablename;
 
   /* Number of indexes */
   uint m_key_count;
@@ -637,7 +637,10 @@ public:
   bool put_dict(Rdb_dict_manager *dict, rocksdb::WriteBatch *batch,
                 uchar *key, size_t keylen);
 
-  void set_name(const char *name, size_t len);
+  void set_name(const char *name, size_t len) {
+    set_name(std::string(name, len));
+  }
+  void set_name(const std::string& name);
   void set_name(const rocksdb::Slice& slice, size_t pos = 0) {
     set_name(slice.data() + pos, slice.size() - pos);
   }
@@ -689,7 +692,7 @@ class Rdb_ddl_manager
   Rdb_dict_manager *m_dict= nullptr;
   my_core::HASH m_ddl_hash;  // Contains Rdb_tbl_def elements
   // maps index id to <table_name, index number>
-  std::map<GL_INDEX_ID, std::pair<std::basic_string<uchar>, uint>>
+  std::map<GL_INDEX_ID, std::pair<std::string, uint>>
     m_index_num_to_keydef;
   mysql_rwlock_t m_rwlock;
 
@@ -705,7 +708,7 @@ public:
 
   void cleanup();
 
-  Rdb_tbl_def* find(const uchar *table_name, uint len, bool lock= true);
+  Rdb_tbl_def* find(const std::string& table_name, bool lock= true);
   const std::shared_ptr<Rdb_key_def>& find(GL_INDEX_ID gl_index_id);
   std::shared_ptr<Rdb_key_def> safe_find(GL_INDEX_ID gl_index_id);
   void set_stats(
@@ -719,7 +722,7 @@ public:
   /* Modify the mapping and write it to on-disk storage */
   int put_and_write(Rdb_tbl_def *key_descr, rocksdb::WriteBatch *batch);
   void remove(Rdb_tbl_def *rec, rocksdb::WriteBatch *batch, bool lock= true);
-  bool rename(uchar *from, uint from_len, uchar *to, uint to_len,
+  bool rename(const std::string& from, const std::string& to,
               rocksdb::WriteBatch *batch);
 
   uint get_and_update_next_number(Rdb_dict_manager *dict)
@@ -735,8 +738,8 @@ private:
   int put(Rdb_tbl_def *key_descr, bool lock= true);
 
   /* Helper functions to be passed to my_core::HASH object */
-  static uchar* get_hash_key(Rdb_tbl_def *rec, size_t *length,
-                             my_bool not_used __attribute__((unused)));
+  static const uchar* get_hash_key(Rdb_tbl_def *rec, size_t *length,
+                                   my_bool not_used __attribute__((unused)));
   static void free_hash_elem(void* data);
 
   bool validate_schemas();

facebook/mysql-8.0

storage/rocksdb/ha_rocksdb.h
@@ -377,10 +377,10 @@ class ha_rocksdb: public my_core::handler
   */
   int m_dupp_errkey;
 
-  int create_key_defs(TABLE *table_arg, const char *db_table, const uint len,
+  int create_key_defs(const TABLE *table_arg, const std::string& db_table,
                       HA_CREATE_INFO *create_info, Rdb_tbl_def *tbl_def_arg,
-                      Rdb_tbl_def *old_tbl_def_arg= nullptr)
-      MY_ATTRIBUTE((__nonnull__(2, 3, 5, 6), __warn_unused_result__));
+                      const Rdb_tbl_def *old_tbl_def_arg= nullptr)
+      MY_ATTRIBUTE((__nonnull__(2, 4, 5), __warn_unused_result__));
   int secondary_index_read(const int keyno, uchar *buf)
       MY_ATTRIBUTE((__nonnull__, __warn_unused_result__));
   void setup_iterator_for_rnd_scan();
@@ -701,18 +701,19 @@ private:
     bool             skip_unique_check;
   };
 
-  int create_cfs(TABLE *table_arg, const char *db_table,
+  int create_cfs(const TABLE *table_arg, const std::string& db_table,
                  Rdb_tbl_def *tbl_def_arg,
                  std::array<struct key_def_cf_info, MAX_INDEXES + 1>* cfs);
 
 
-  int create_key_def(TABLE *table_arg, uint i, const Rdb_tbl_def* tbl_def_arg,
+  int create_key_def(const TABLE *table_arg, uint i,
+                     const Rdb_tbl_def* tbl_def_arg,
                      std::shared_ptr<Rdb_key_def>* new_key_def,
                      const struct key_def_cf_info& cf_info);
 
-  int create_inplace_key_defs(TABLE *table_arg,
+  int create_inplace_key_defs(const TABLE *table_arg,
                        Rdb_tbl_def *tbl_def_arg,
-                       Rdb_tbl_def *old_tbl_def_arg,
+                       const Rdb_tbl_def *old_tbl_def_arg,
                        const std::array<key_def_cf_info, MAX_INDEXES + 1>& cfs);
 
   int index_first_intern(uchar *buf)

storage/rocksdb/rdb_datadic.h
@@ -344,7 +344,7 @@ public:
     SECONDARY_FORMAT_VERSION_LATEST= SECONDARY_FORMAT_VERSION_INITIAL,
   };
 
-  void setup(TABLE *table, Rdb_tbl_def *tbl_def);
+  void setup(const TABLE *table, const Rdb_tbl_def *tbl_def);
 
   rocksdb::ColumnFamilyHandle *get_cf() const { return m_cf_handle; }
 
@@ -620,7 +620,7 @@ public:
   ~Rdb_tbl_def();
 
   /* Stores 'dbname.tablename' */
-  StringBuffer<64> m_dbname_tablename;
+  std::string m_dbname_tablename;
 
   /* Number of indexes */
   uint m_key_count;
@@ -637,7 +637,10 @@ public:
   bool put_dict(Rdb_dict_manager *dict, rocksdb::WriteBatch *batch,
                 uchar *key, size_t keylen);
 
-  void set_name(const char *name, size_t len);
+  void set_name(const char *name, size_t len) {
+    set_name(std::string(name, len));
+  }
+  void set_name(const std::string& name);
   void set_name(const rocksdb::Slice& slice, size_t pos = 0) {
     set_name(slice.data() + pos, slice.size() - pos);
   }
@@ -689,7 +692,7 @@ class Rdb_ddl_manager
   Rdb_dict_manager *m_dict= nullptr;
   my_core::HASH m_ddl_hash;  // Contains Rdb_tbl_def elements
   // maps index id to <table_name, index number>
-  std::map<GL_INDEX_ID, std::pair<std::basic_string<uchar>, uint>>
+  std::map<GL_INDEX_ID, std::pair<std::string, uint>>
     m_index_num_to_keydef;
   mysql_rwlock_t m_rwlock;
 
@@ -705,7 +708,7 @@ public:
 
   void cleanup();
 
-  Rdb_tbl_def* find(const uchar *table_name, uint len, bool lock= true);
+  Rdb_tbl_def* find(const std::string& table_name, bool lock= true);
   const std::shared_ptr<Rdb_key_def>& find(GL_INDEX_ID gl_index_id);
   std::shared_ptr<Rdb_key_def> safe_find(GL_INDEX_ID gl_index_id);
   void set_stats(
@@ -719,7 +722,7 @@ public:
   /* Modify the mapping and write it to on-disk storage */
   int put_and_write(Rdb_tbl_def *key_descr, rocksdb::WriteBatch *batch);
   void remove(Rdb_tbl_def *rec, rocksdb::WriteBatch *batch, bool lock= true);
-  bool rename(uchar *from, uint from_len, uchar *to, uint to_len,
+  bool rename(const std::string& from, const std::string& to,
               rocksdb::WriteBatch *batch);
 
   uint get_and_update_next_number(Rdb_dict_manager *dict)
@@ -735,8 +738,8 @@ private:
   int put(Rdb_tbl_def *key_descr, bool lock= true);
 
   /* Helper functions to be passed to my_core::HASH object */
-  static uchar* get_hash_key(Rdb_tbl_def *rec, size_t *length,
-                             my_bool not_used MY_ATTRIBUTE((unused)));
+  static const uchar* get_hash_key(Rdb_tbl_def *rec, size_t *length,
+                                   my_bool not_used MY_ATTRIBUTE((unused)));
   static void free_hash_elem(void* data);
 
   bool validate_schemas();