facebook/mysql-5.6

fix index_length statistics for secondary keys created via online ddl
Summary:
This diff addresses fixing index_length statistics for secondary keys via
online ddl.  Previously, the statistics to be adjusted by the ddl_manager at
the end of each IngestExternalFile call were failing silently, because the
gl_index_id for the index ongoing creation was not yet in the data dictionary.

This fixes the issue by essentially running analyze table at the end of
commit_inplace_alter_table

Reviewed By: yoshinorim

Differential Revision: D4324056

fbshipit-source-id: 4dbf820

storage/rocksdb/ha_rocksdb.h
@@ -997,6 +997,8 @@ public:
     __attribute__((__warn_unused_result__));
   int analyze(THD* thd, HA_CHECK_OPT* check_opt)
     __attribute__((__warn_unused_result__));
+  int calculate_stats(const TABLE* table_arg, THD* thd, HA_CHECK_OPT* check_opt)
+    __attribute__((__warn_unused_result__));
 
   enum_alter_inplace_result check_if_supported_inplace_alter(
     TABLE *altered_table,

facebook/mysql-8.0

storage/rocksdb/ha_rocksdb.h
@@ -997,6 +997,8 @@ public:
     MY_ATTRIBUTE((__warn_unused_result__));
   int analyze(THD* thd, HA_CHECK_OPT* check_opt)
     MY_ATTRIBUTE((__warn_unused_result__));
+  int calculate_stats(const TABLE* table_arg, THD* thd, HA_CHECK_OPT* check_opt)
+    MY_ATTRIBUTE((__warn_unused_result__));
 
   enum_alter_inplace_result check_if_supported_inplace_alter(
     TABLE *altered_table,